299a68ef8728e1c713d0e43312f17958a0f8b82c
Fernando Lara <larafernandoagustin@gmail.com>
2023-02-23 00:36:56 -0300

    footer finalizado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Header from './components/header/Header.jsx';
 import Home from './components/home/Home.jsx';
 import Yo from './components/yo/Yo.jsx';
+
+import Footer from './components/footer/Footer.jsx';
 import './App.css';
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <main>
         <Home/>
         <Yo/>
+
+        <Footer/>
       </main>
     </div>
   );
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./footer.css";
+const Footer = () => {
+    return(
+        <footer className="footer">
+            <div className="footer__container container">
+                <h3 className="footer__title">FL</h3>
+                <div className="footer__social">
+                    <a href="https://wa.me/5493516133578" className="footer__social-link" target="_blank"><i class="bx bxl-whatsapp"></i></a>
+
+                    <a href="https://www.linkedin.com/in/fernando-lara-/" className="footer__social-link" target="_blank"><i class="bx bxl-linkedin"></i></a>
+
+                    <a href="https://github.com/Ferlara02" className="footer__social-link" target="_blank"><i class="bx bxl-github"></i></a>
+
+                    <a href="https://twitter.com/anreverso" className="footer__social-link" target="_blank"><i class="bx bxl-twitter"></i></a>
+                </div>
+                <span className="footer__copy">© Fernando Lara. All rights reserved.</span>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
