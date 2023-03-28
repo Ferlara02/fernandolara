ef41a8c71447b35aee87ae0bad30873230144adc
Fernando Lara <larafernandoagustin@gmail.com>
2023-03-28 01:30:08 -0300

    HandleScroll para setActiveNav

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Logo } from "../../assets/logo";
 import "./header.css";
 import SinT from "../../assets/logo.png"
+import useTitle from "../../hooks/useTitle";
 const Header = () => {
     const [Toggle, showMenu] = useState(false);
-
+    
     const [activeNav, setActiveNav] = useState(`#home`);
+
+    //Handle scroll para setear el componente activo al scrollear
+    const handleScroll = () => {
+        const homePos = document.getElementById("home").getBoundingClientRect();
+        const yoPos = document.getElementById("yo").getBoundingClientRect();
+        const portfolioPos = document.getElementById("portfolio").getBoundingClientRect();
+        const contactoPos = document.getElementById("contacto").getBoundingClientRect();
+        
+        if (homePos.top < window.innerHeight / 2 && homePos.bottom > window.innerHeight / 2) {
+          setActiveNav("#home");
+        } else if (yoPos.top < window.innerHeight / 2 && yoPos.bottom > window.innerHeight / 2) {
+          setActiveNav("#yo");
+        } else if (portfolioPos.top < window.innerHeight / 2 && portfolioPos.bottom > window.innerHeight / 2) {
+          setActiveNav("#portfolio");
+        } else if (contactoPos.top < window.innerHeight / 2 && contactoPos.bottom > window.innerHeight / 2) {
+          setActiveNav("#contacto");
+        }
+    };
+      
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    useTitle({title: activeNav})
+
     return(
         <header className="header">
             <nav className="nav container">
@@ -13,7 +40,9 @@ const Header = () => {
                 <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
                     <ul className="nav__list">
                         <li className="nav__item">
-                            <a href="#home" onClick={() => setActiveNav("#home")} className={activeNav === "#home" ? "nav__link active-link" : "nav__link"}>
+                            <a href="#home" onClick={() => {
+                                setActiveNav("#home")
+                                }} className={activeNav === "#home" ? "nav__link active-link" : "nav__link"}>
                                 <i className="uil uil-estate nav__icon"></i>
                                 Home
                             </a>
